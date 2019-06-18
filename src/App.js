f2e07c909d0e155dbb574f70e262c783024e9a28
Mihal <mih3404@mail.ru>
2019-06-18 10:56:03 +0400

    Нов

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      counter: 0
+    };
+  }
+  handleClick(){
+    this.setState({
+      counter: this.state.counter+1 
+    });
+  }
+
+ 
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Сетчик сейчас равен {this.state.counter}
+        </p>
+        <button type="button" onClick={this.handleClick.bind(this)} >Увеличить счетик</button>
+      </header>
+    </div>
+  );
+  }
+}
+
+export default App;
