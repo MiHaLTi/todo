d969ddb3c1d35d7cc5d5ccb17d0573e668fc52dc
Mihal <mih3404@mail.ru>
2019-06-20 11:52:12 +0400

    11:51

--- src/Todos.js
+++ src/Todos.js
@@ -22,6 +22,49 @@ export class Todos extends React.Component {
         });
     }
 
+    handleDelete(id) {
+        this.setState({
+            tasks: this.state.tasks.filter(task => task.id !== id)
+        });
+    }
+
+    handleComplete(id) {
+        const { tasks } = this.state;
+        const completedTask = tasks.find(task => task.id === id);
+
+        if (completedTask === undefined) {
+            return;
+        }
+
+        if (completedTask.completed === true) {
+            completedTask.completed = false;
+
+            this.setState({
+                tasks: [
+                    completedTask,
+                    ...tasks.filter(task => task.id !== id)
+                ]
+            });
+
+            return;
+        }
+
+        completedTask.completed = true;
+
+        this.setState({
+            tasks: [
+                ...tasks.filter(task => task.id !== id),
+                completedTask
+            ]
+        });
+    }
+
+    handleOnEnterPress(event) {
+        if (event.key === 'Enter') {
+            this.handleAddTask();
+        }
+    }
+
     handleAddTask() {
         const { newTaskText } = this.state;
         
@@ -46,10 +89,23 @@ export class Todos extends React.Component {
         const { tasks, newTaskText } = this.state;
 
         return <div className="todos">
-            <input value={newTaskText} onChange={this.handleChange.bind(this)} type="text" placeholder="Введите новую задачу"></input>
-            <button onClick={this.handleAddTask.bind(this)} type="button">Добавить</button>
+            <div className="add-todo">
+                <input 
+                    value={newTaskText}
+                    onChange={this.handleChange.bind(this)}
+                    onKeyPress={this.handleOnEnterPress.bind(this)}
+                    type="text"
+                    placeholder="Введите новую задачу"
+                ></input>
+                <i className="fas fa-plus" onClick={this.handleAddTask.bind(this)}></i>
+            </div>
             <ul>
-                {tasks.map(task => <Todo key={task.id} task={task}></Todo>)}
+                {tasks.map(task => <Todo
+                    key={task.id}
+                    task={task}
+                    onDelete={this.handleDelete.bind(this)}
+                    onComplete={this.handleComplete.bind(this)}
+                    ></Todo>)}
             </ul>
         </div>;
     }
